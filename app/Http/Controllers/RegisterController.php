881b78e0d9ccf95ac34c9db5a31974ec2111de74
Muhammad Mu'min Azis <muhammadmuminaziz@gmail.com>
2022-09-05 04:40:24 -0700

    secon saved

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,21 +14,9 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(Request $request)
+    public function store(RegisterRequest $request)
     {
-        $request->validate([
-            'name' => 'required|string|min:3',
-            'username' => 'required|string|min:3|max:25|alpha_num',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:3'
-        ]);
-
-        User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-        return redirect('/');
+        User::create($request->all());
+        return redirect('/')->with('success', 'Thank You... You are now registered.');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,20 @@
 <x-app-layout>
-    <h1>Ini adalah halaman Home</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                @if(session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+                <div class="card">
+                    @endif
+                    <div class="card-header">Welcome to my site</div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit atque nulla, ut nemo eius harum provident nesciunt assumenda, at, unde ipsa in aliquam quisquam nobis exercitationem eos ullam illum neque.</p>
+                        <p>Nemo rerum magnam reiciendis sunt earum quam doloremque eius ab vel, tenetur suscipit, dolorum dolore totam hic laboriosam incidunt commodi maiores perferendis ducimus enim repellendus. Ipsa veritatis iure voluptatum impedit?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
